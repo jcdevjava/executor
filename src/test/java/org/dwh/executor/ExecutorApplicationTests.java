659b9961274d310711a68eedb4abd0e3c016839c
José Carlos Palma <palmahn@gmail.com>
2022-07-24 14:49:34 -0600

    init(app): Creación del proyecto.

---
+++ src/test/java/org/dwh/executor/ExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dwh.executor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
