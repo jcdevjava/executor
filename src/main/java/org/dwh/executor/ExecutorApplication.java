f82271e426be17adb0784b4f132510ef033ff6db
José Carlos Palma <palmahn@gmail.com>
2022-07-24 14:52:34 -0600

    config(app): Habilitación de métodos asíncronos.

--- src/main/java/org/dwh/executor/ExecutorApplication.java
+++ src/main/java/org/dwh/executor/ExecutorApplication.java
@@ -2,12 +2,29 @@ package org.dwh.executor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class ExecutorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExecutorApplication.class, args);
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(10);
+        executor.setThreadNamePrefix("executor-");
+        executor.initialize();
+        return executor;
     }
 
 }
